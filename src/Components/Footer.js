29466eabc54d610c2e3c1ac92d492d29f88e6a0a
Rahul Singh <rkant225@gmail.com>
2021-10-23 20:28:13 +0530

    Added code changes to open source code in new tab.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div style={{width : '100%', textAlign : 'right', marginBottom : '.5rem'}}>
             <hr/>
-            <a href="https://github.com/rkant225/star_patterns">Source Code</a>
+            <a href="https://github.com/rkant225/star_patterns" rel="noreferrer" target="_blank">Source Code</a>
         </div>
     )
 }
